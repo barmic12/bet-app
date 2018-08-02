09b0da0b8a460b9d6cb619aef3f2ccba986f40f4
barmic12_2 <218481@student.pwr.edu.pl>
2018-08-02 17:43:46 +0200

    Mailer configuration.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,11 +27,22 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  ActionMailer::Base.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => ENV['EMAIL_USERNAME'],
+      :password             => ENV['EMAIL_PASSWORD'],
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
