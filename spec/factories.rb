b8b7b2a98910d0913c57dde1a6cfa140559bc849
barmic12_2 <218481@student.pwr.edu.pl>
2018-08-02 18:23:14 +0200

    Add helper for session handling.

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,15 @@ FactoryBot.define do
     score "2:1"
     date "2018-07-24 18:00:00"
   end
+
+  factory :user do
+    email "example@example.com"
+    password "password"
+    trait :normal do
+      role 0
+    end
+    trait :admin do
+      role 1
+    end
+  end
 end
